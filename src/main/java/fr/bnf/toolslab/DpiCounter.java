4f0a8741d8471b8136819f085c01f541465de380
Thomas Ledoux <tledouxfr@gmail.com>
2022-02-12 12:21:47 +0100

    Stream-based dpi counter

--- src/main/java/fr/bnf/toolslab/DpiCounter.java
+++ src/main/java/fr/bnf/toolslab/DpiCounter.java
@@ -1,63 +1,63 @@
 package fr.bnf.toolslab;
 
 import java.util.AbstractMap;
-import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.stream.Collectors;
 
 /**
- * Counter to classify the different values.
- * In order to group the value, only he multiple of 10 are kept.
+ * Counter to classify the different values. In order to group the values, only
+ * multiples of 10 are kept, using the nearest value.
  *
  */
 public class DpiCounter {
 
 	Map<Integer, Integer> dpis = new HashMap<>();
 
+	/**
+	 * Get the nearest ten
+	 * 
+	 * @param value
+	 * @return rounded value
+	 */
 	private int getRoundedValue(int value) {
-		return (value / 10) * 10;
+		return ((value + 5) / 10) * 10;
 	}
 
 	public void increment(int dpi) {
 		int rounded = getRoundedValue(dpi);
-		Integer previous = dpis.get(rounded);
-		if (previous == null) {
-			dpis.put(rounded, 1);
-		} else {
-			dpis.put(rounded, previous.intValue() + 1);
-		}
+		dpis.merge(Integer.valueOf(rounded), 1, Integer::sum);
 	}
 
 	public int get(int dpi) {
-		Integer previous = dpis.get(getRoundedValue(dpi));
-		if (previous == null) {
-			return 0;
-		}
-		return previous.intValue();
+		return dpis.getOrDefault(dpi, 0);
 	}
 
+	/**
+	 * Return the most usual dpi
+	 * 
+	 * @return Entry containing the most usual dpi and the number of occurrences
+	 */
 	public Entry<Integer, Integer> getBest() {
-		// Find the most usual dpi
-		int maxNumber = 0;
-		Entry<Integer, Integer> best = new AbstractMap.SimpleEntry<>(0, 0);
-		for (Entry<Integer, Integer> e : dpis.entrySet()) {
-			if (e.getValue().intValue() > maxNumber) {
-				best = new AbstractMap.SimpleEntry<>(e.getKey(), e.getValue());
-				maxNumber = e.getValue().intValue();
-			}
-		}
-		return best;
+		final Comparator<Map.Entry<Integer, Integer>> valueComparator = Map.Entry
+				.comparingByValue(Comparator.reverseOrder());
+		final Entry<Integer, Integer> defaultValue = new AbstractMap.SimpleEntry<>(0, 0);
+		return dpis.entrySet().stream().sorted(valueComparator).findFirst()
+				.orElseGet(() -> defaultValue);
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
 	public String toString() {
-		List<String> parts = new ArrayList<>();
-		for (Entry<Integer, Integer> e : dpis.entrySet()) {
-			parts.add("DPI(" + e.getKey() + ")=" + e.getValue());
-		}
-		return String.join("; ",parts);
-		
-		
+		final Comparator<Map.Entry<Integer, Integer>> keyComparator = Map.Entry
+				.comparingByKey(Comparator.naturalOrder());
+		return dpis.entrySet().stream().sorted(keyComparator)
+				.map(e -> "DPI(" + e.getKey() + ")=" + e.getValue())
+				.collect(Collectors.joining("; "));
 	}
 }
--- src/test/java/fr/bnf/toolslab/DpiCounterTest.java
+++ src/test/java/fr/bnf/toolslab/DpiCounterTest.java
@@ -12,50 +12,89 @@ public class DpiCounterTest {
 	protected static Logger LOGGER = Logger.getLogger(DpiCounterTest.class
 			.getName());
 
+	DpiCounter counter;
+
 	@Before
 	public void setUp() throws Exception {
+		counter = new DpiCounter();
 	}
 
 	@Test
 	public void testIncrement() {
-		DpiCounter test1 = new DpiCounter();
-		LOGGER.info(test1.toString());
-		assertEquals(0, test1.get(200));
+		LOGGER.info(counter.toString());
+		assertEquals(0, counter.get(200));
 
-		test1.increment(100);
-		LOGGER.info(test1.toString());
-		assertEquals(1, test1.get(100));
-		assertEquals(0, test1.get(200));
+		counter.increment(96);
+		LOGGER.info(counter.toString());
+		assertEquals(1, counter.get(100));
+		assertEquals(0, counter.get(200));
 
-		test1.increment(205);
-		LOGGER.info(test1.toString());
-		assertEquals(1, test1.get(100));
-		assertEquals(1, test1.get(200));
+		counter.increment(204);
+		LOGGER.info(counter.toString());
+		assertEquals(1, counter.get(100));
+		assertEquals(1, counter.get(200));
 
-		test1.increment(102);
-		LOGGER.info(test1.toString());
-		assertEquals(2, test1.get(100));
-		assertEquals(1, test1.get(200));
+		counter.increment(102);
+		LOGGER.info(counter.toString());
+		assertEquals(2, counter.get(100));
+		assertEquals(1, counter.get(200));
+	}
+
+	@Test
+	public void testGet() {
+		assertEquals(0, counter.get(200));
+		counter.increment(104);
+		assertEquals(0, counter.get(200));
+		assertEquals(1, counter.get(100));
+		counter.increment(199);
+		counter.increment(201);
+		assertEquals(2, counter.get(200));
+		assertEquals(1, counter.get(100));
+		assertEquals(0, counter.get(210));
 	}
 
 	@Test
 	public void testGetBest() {
-		DpiCounter test1 = new DpiCounter();
-		Entry<Integer, Integer> best1 = test1.getBest();
-		LOGGER.info(test1.toString());
+		Entry<Integer, Integer> best1 = counter.getBest();
+		LOGGER.info(counter.toString());
 		assertEquals(0, best1.getKey().intValue());
 
-		test1.increment(101);
-		test1.increment(102);
-		test1.increment(103);
-		test1.increment(104);
-		test1.increment(105);
-		test1.increment(200);
-		test1.increment(200);
-		Entry<Integer, Integer> best2 = test1.getBest();
-		LOGGER.info(test1.toString());
+		// All 110
+		counter.increment(105);
+		// All 200
+		counter.increment(199);
+		counter.increment(201);
+		// All 100
+		counter.increment(95);
+		counter.increment(96);
+		counter.increment(97);
+		counter.increment(98);
+		counter.increment(99);
+		counter.increment(100);
+		counter.increment(101);
+		counter.increment(102);
+		counter.increment(103);
+		counter.increment(104);
+		Entry<Integer, Integer> best2 = counter.getBest();
+		LOGGER.info(counter.toString());
 		assertEquals(100, best2.getKey().intValue());
-		assertEquals(5, best2.getValue().intValue());
+		assertEquals(10, best2.getValue().intValue());
+	}
+
+	@Test
+	public void testtoString() {
+		assertEquals("", counter.toString());
+
+		counter.increment(105);
+		assertEquals("DPI(110)=1", counter.toString());
+		counter.increment(199);
+		assertEquals("DPI(110)=1; DPI(200)=1", counter.toString());
+		counter.increment(97);
+		assertEquals("DPI(100)=1; DPI(110)=1; DPI(200)=1", counter.toString());
+		counter.increment(201);
+		assertEquals("DPI(100)=1; DPI(110)=1; DPI(200)=2", counter.toString());
+		counter.increment(95);
+		assertEquals("DPI(100)=2; DPI(110)=1; DPI(200)=2", counter.toString());
 	}
 
 }
